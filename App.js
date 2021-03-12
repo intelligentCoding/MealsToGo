3706d579dc65ada28f8728162d2b855de2b5b6c3
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-03-12 09:40:25 -0700

    Theme integeration

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { RestuarantScreen } from "./src/features/restaurants/screens/restaurant-info.screen";
-
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme";
 export default function App() {
   return (
     <>
-      <RestuarantScreen />
+      <ThemeProvider theme={theme}>
+        <RestuarantScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
