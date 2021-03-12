6e5f9980471eb003f32cb2754dffc72396580997
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-03-12 08:48:06 -0700

    Migrated to Styled Components

--- src/features/restaurants/screens/restaurant-info.screen.js
+++ src/features/restaurants/screens/restaurant-info.screen.js
@@ -3,24 +3,31 @@ import React from "react";
 import { StatusBar, SafeAreaView, StyleSheet, View } from "react-native";
 
 import { Searchbar } from "react-native-paper";
+import styled from "styled-components/native";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const SearchContainer = styled.View`
+  padding: 16px;
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: white;
+`;
 export const RestuarantScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <View style={styles.search}>
+  <SafeArea>
+    <SearchContainer>
       <Searchbar />
-    </View>
-    <View style={styles.list}>
+    </SearchContainer>
+    <RestaurantListContainer>
       <RestaurantInfoCard />
-    </View>
-  </SafeAreaView>
+    </RestaurantListContainer>
+  </SafeArea>
 );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: { padding: 16, backgroundColor: "green" },
-  list: { flex: 1, padding: 16, backgroundColor: "blue" },
-});
