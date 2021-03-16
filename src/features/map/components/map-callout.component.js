0b1bf4d4932b3340527ab23f9e19d311cd6e7b37
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-03-15 20:52:07 -0600

    map call out with map-callout component

---
+++ src/features/map/components/map-callout.component.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styled from "styled-components/native";
+const MyText = styled.Text``;
+export const MapCallout = ({ restaurant }) => (
+  <MyText>{restaurant.name}</MyText>
+);
--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,14 +1,17 @@
 import React, { useContext, useState, useEffect } from "react";
+import { View } from "react-native";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { Search } from "../components/search.component";
+import { MapCallout } from "../components/map-callout.component";
 const Map = styled(MapView)`
   height: 100%;
   width: 100%;
 `;
 
+const SomeText = styled.Text``;
 export const MapsScreen = () => {
   const { location } = useContext(LocationContext);
   const { restaurants = [] } = useContext(RestaurantsContext);
@@ -40,7 +43,11 @@ export const MapsScreen = () => {
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
               }}
-            />
+            >
+              <MapView.Callout>
+                <MapCallout restaurant={restaurant} />
+              </MapView.Callout>
+            </MapView.Marker>
           );
         })}
       </Map>
