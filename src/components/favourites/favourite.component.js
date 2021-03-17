b88d42a3298917ab1a911ccaeeebc34b57c18a15
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-03-16 22:17:22 -0600

    Favourite component initial setup

--- src/components/favourites/favourite.component.js
+++ src/components/favourites/favourite.component.js
@@ -1,3 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FavouritesContext } from "../../services/favourites/favourites.context";
+import styled from "styled-components/native";
+import { TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
-export const Favourite = () => null;
+const FavouriteButton = styled(TouchableOpacity)`
+  position: absolute;
+  top: 24px;
+  right: 10px;
+  z-index: 9;
+`;
+export const Favourite = () => {
+  const { favourites, addToFavourites, removeFromFavourites } = useContext(
+    FavouritesContext
+  );
+  return (
+    <FavouriteButton>
+      <AntDesign name="heart" size={24} color="red" />
+    </FavouriteButton>
+  );
+};
