e7dcb2eff7bf2ad90879993ca5d7ea688f4130cd
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-03-17 00:18:47 -0600

    android mapview bug fixed

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -2,5 +2,5 @@ import React from "react";
 import styled from "styled-components/native";
 import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info";
 export const MapCallout = ({ restaurant }) => (
-  <CompactRestaurantInfo restaurant={restaurant} />
+  <CompactRestaurantInfo restaurant={restaurant} isMap />
 );
